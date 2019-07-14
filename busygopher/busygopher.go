9abe8050f446e0dc5c59fd4bee7be3d05377bb28
Francesco Farina <rockerg991@gmail.com>
2019-07-15 00:09:54 +0100

    initial skeleton of the Step function

--- busygopher/busygopher.go
+++ busygopher/busygopher.go
@@ -1,22 +1,36 @@
 package busygopher
 
-import "io"
-
+// Op is an operation that the machine can perform.
 type Op struct {
-	write rune
-	move  rune
-	stop  rune
+	Write int
+	Move  int
+	Stop  int
+	Next  int
 }
+
+// Tape where the input
 type Tape interface {
-	io.ReadWriteSeeker
+	ReadHead() int
+	WriteHead(int)
+	MoveHead(int)
 }
 
 type Deck interface {
-	Card(int) Op
+	Card(int) [2]Op
 }
 type BusyGopher struct {
 	Cards  Deck
+	Tape   Tape
+	State  int
 	Halted bool
 }
 
-func (bg *BusyGopher) Step(rune c) {}
+func (bg *BusyGopher) Step(c int) {
+	op := bg.Cards.Card(bg.State)[c]
+	if op.Stop == 1 {
+		return // halt
+	}
+	bg.Tape.WriteHead(op.Write)
+	bg.Tape.MoveHead(op.Move)
+	bg.State = op.Next
+}
